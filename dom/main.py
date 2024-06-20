33e24e991bedd50c75e04d151f4e94d77a4249fe
vladbot2 <vladsidlar434@gmail.com>
2024-06-20 17:50:11 +0300

    Add files via upload

---
+++ dom/main.py
@@ -0,0 +1,53 @@
+# 1
+# start = int(input("5: "))
+# end = int(input("20: "))
+# for number in range(start, end + 1):
+#     if number % 5 == 0:
+#         print(number)
+
+# 2
+# start = int(input("8: "))
+# end = int(input("14: "))
+
+# # 1
+# print("8 9 10 11 12 13 14:")
+# for number in range(start, end + 1):
+#     print(number, end=' ')
+# print()
+
+# # 2
+# print("14 13 12 11 10 9 8:")
+# for number in range(end, start - 1, -1):
+#     print(number, end=' ')
+# print()
+
+# # 3
+# print("1:")
+# for number in range(start, end + 1):
+#     if number % 7 == 0:
+#         print(number, end=' ')
+# print()
+
+# # 4
+# count_multiple_5 = 0
+# for number in range(start, end + 1):
+#     if number % 5 == 0:
+#         count_multiple_5 += 1
+# print("1:", count_multiple_5)
+
+# 3
+# start = int(input("6: "))
+# end = int(input("15: "))
+# for number in range(start, end + 1):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("15")
+#     elif number % 3 == 0:
+#         print("3")
+#     elif number % 5 == 0:
+#         print("3")
+#     else:
+#         print(number)
+
+
+ 
+ 
